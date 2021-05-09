6f42052a5500ef1d4645d5fdebea82960e2cd219
Roberto Carlos Jarquín Mena <rjarquin@cgclatam.com>
2021-05-09 14:12:46 -0600

    setup for heroku

--- MovieApp/settings.py
+++ MovieApp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 
@@ -153,3 +153,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+django_heroku.settings(locals())
