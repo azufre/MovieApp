c62b4beeaef395045f5cf959cd27e30aed44c0a8
Roberto Carlos Jarquín Mena <rjarquin@cgclatam.com>
2021-05-09 13:48:13 -0600

    Allow host

--- MovieApp/settings.py
+++ MovieApp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'rl!94nv&5vl2y7=g+$#m52s5s(mb)#*t-=vm7!xmknz!=*u*#z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://appmovietestbackend.herokuapp.com/']
 
 
 # Application definition
